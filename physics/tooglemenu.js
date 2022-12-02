0d2ee05e5aa534ec5fa82ffba4fe6c4f08e4dcda
Alex-pixelui <119135268+Alex-pixelui@users.noreply.github.com>
2022-12-02 18:36:37 +0300

    sliding menu
    Sliding bar was still visible when page is zoomed out.
    Display settings adjusted to ensure that hidden side bar is invisible at -300px

--- physics/tooglemenu.js
+++ physics/tooglemenu.js
@@ -2,7 +2,9 @@ var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
     navLinks.style.right = "0";
+    navLinks.style.display = "block";
 }
 function hideMenu() {
     navLinks.style.right = "-300px";
+    navLinks.style.display = "none";
 }
